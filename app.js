5dff8e3d352f7fe73c246e9c16a1c67d3f7889eb
chrissDev <dushimec515@gmail.com>
2023-11-09 13:06:15 +0100

    Programmer gigs application

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const path = require('path')
+const db = require('./config/database')
+
+
+const app = express()
+const PORT = process.env.PORT || 8000
+app.use('/gigs', require('./routes/gig'))
+db.authenticate()
+ .then(() => console.log('Db connected!'))
+ .catch((err) => console.log('Error'+err))
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', (req,res) =>{
+    res.send('Welcome on my Servser') 
+})
+
+app.listen(PORT, console.log(`Sever is running on http://localhost:${PORT}`))
